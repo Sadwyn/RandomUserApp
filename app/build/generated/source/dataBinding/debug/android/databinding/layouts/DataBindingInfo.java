b54670ea9c072ffd28f07bf022de5327b927d35e
Sadwyn <sadwyn1@gmail.com>
2017-07-02 18:36:08 +0300

    toolbar theme changed

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="cce22fbf-0c6a-441d-8f34-e7a9332e308c")
+@BindingBuildInfo(buildId="3453b2e0-3a19-43f3-8a66-e4ced7237409")
 public class DataBindingInfo {}
